9c411b31e2d3ac1537e85df23087c5bef9f68eef
Rizvi <rizvida1@gmail.com>
2019-11-25 10:51:12 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,12 +3,12 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
-const str = process.env.PGHOST;
+// const str = process.env.PGHOST;
 
-const host = str.substring(0, str.lastIndexOf(":"));
-const port = str.substring(str.lastIndexOf(":") + 1, str.length);
-console.log(host);
-console.log(port);
+// const host = str.substring(0, str.lastIndexOf(":"));
+// const port = str.substring(str.lastIndexOf(":") + 1, str.length);
+// console.log(host);
+// console.log(port);
 console.log("PGHOST:", process.env.PGHOST);
 
 // Option 1: Passing parameters separately
