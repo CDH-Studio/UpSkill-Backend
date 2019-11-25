939cbd719451de4d253831a381177ed2b0d7f8fd
Rizvi <rizvida1@gmail.com>
2019-11-25 10:59:30 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,29 +3,34 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
-// const str = process.env.PGHOST;
+const str = process.env.DATABASE_URL;
 
 // const host = str.substring(0, str.lastIndexOf(":"));
 // const port = str.substring(str.lastIndexOf(":") + 1, str.length);
-// console.log(host);
-// console.log(port);
-console.log("PGHOST:", process.env.PGHOST);
+
+let [dialect, host, port] = str.split(":");
+
+host = host.replace("//", "");
+
+console.log(dialect);
+console.log(host);
+console.log(port);
 
 // Option 1: Passing parameters separately
 module.exports = new Sequelize(
-  process.env.DATABASE_URL
-  // process.env.PGDATABASE,
-  // process.env.PGUSERNAME,
-  // process.env.PGPASS,
-  // {
-  //   host: host,
-  //   port: port,
-  //   dialect: "postgres",
-  //   pool: {
-  //     max: 5,
-  //     min: 0,
-  //     acquire: 30000,
-  //     idle: 10000
-  //   }
-  // }
+  // process.env.DATABASE_URL
+  process.env.PGDATABASE,
+  process.env.PGUSERNAME,
+  process.env.PGPASS,
+  {
+    host: host,
+    port: port,
+    dialect: dialect,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
 );
