ebb635d924929f474d51b5590b7dc1b11ae3cff2
Rizvi <rizvida1@gmail.com>
2019-11-25 10:49:13 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -13,19 +13,19 @@ console.log("PGHOST:", process.env.PGHOST);
 
 // Option 1: Passing parameters separately
 module.exports = new Sequelize(
-  // process.env.DATABASE_URL,
-  process.env.PGDATABASE,
-  process.env.PGUSERNAME,
-  process.env.PGPASS,
-  {
-    host: host,
-    port: port,
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  }
+  process.env.DATABASE_URL
+  // process.env.PGDATABASE,
+  // process.env.PGUSERNAME,
+  // process.env.PGPASS,
+  // {
+  //   host: host,
+  //   port: port,
+  //   dialect: "postgres",
+  //   pool: {
+  //     max: 5,
+  //     min: 0,
+  //     acquire: 30000,
+  //     idle: 10000
+  //   }
+  // }
 );
