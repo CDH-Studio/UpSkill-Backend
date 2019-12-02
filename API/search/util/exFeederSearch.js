4081fc69280dd0bafefedd972ad64d7a9a5d7a4e
Mamadou Bah <bmama067@uottawa.ca>
2019-12-02 15:50:47 -0500

    Cleaned exFeederSearch function

--- API/search/util/exFeederSearch.js
+++ API/search/util/exFeederSearch.js
@@ -1,15 +1,5 @@
 const exFeederSearch = profiles => {
   let results = profiles.filter(profile => profile.exFeeder === true);
-
-  //   profiles.forEach(profile => {
-  //     if (!profile.exFeeder) return;
-  //     const exFeederBool = profile.exFeeder;
-
-  //     // if (exFeederArray.includes(exFeederBool)) exFeederProf.push(profile);
-  //     const result = words.filter(word => word.length > 6);
-  //   });
-  //   return exFeederProf;
-
   return results;
 };
 
