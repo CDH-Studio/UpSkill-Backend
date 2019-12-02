fe5fc2f78d550e54ffad8ff9c918e12a5baf006d
Mamadou Bah <bmama067@uottawa.ca>
2019-12-02 10:41:28 -0500

    Cleaning up seedData

--- seeders/20191105143727-second_language_proficiencies.js
+++ seeders/20191105143727-second_language_proficiencies.js
@@ -6,63 +6,62 @@ module.exports = {
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
     */
-    return queryInterface
-      .bulkInsert(
-        "secondLanguageProficiencies",
-        [
-          {
-            readingProficiency: "C",
-            writingProficiency: "B",
-            oralProficiency: "C",
-            readingDate: new Date(),
-            writingDate: new Date(),
-            oralDate: new Date(),
-            createdAt: new Date(),
-            updatedAt: new Date()
-          },
-          {
-            readingProficiency: "A",
-            writingProficiency: "B",
-            oralProficiency: "C",
-            readingDate: new Date(),
-            writingDate: new Date(),
-            oralDate: new Date(),
-            createdAt: new Date(),
-            updatedAt: new Date()
-          }
-        ],
-        {}
-      )
-      .then(() => {
-        return queryInterface.bulkInsert(
-          "secondLanguageProficiencies",
-          [
-            {
-              id: "91485e4c-ffdb-11e9-8d71-362b9e155667",
-              readingProficiency: "A",
-              writingProficiency: "C",
-              oralProficiency: "B",
-              readingDate: new Date(),
-              writingDate: new Date(),
-              oralDate: new Date(),
-              createdAt: new Date(),
-              updatedAt: new Date()
-            },
-            {
-              id: "a34e38b4-ffdb-11e9-8d71-362b9e155667",
-              readingProficiency: "A",
-              writingProficiency: "B",
-              oralProficiency: "A",
-              readingDate: new Date(),
-              writingDate: new Date(),
-              oralDate: new Date(),
-              createdAt: new Date(),
-              updatedAt: new Date()
-            }
-          ],
-          {}
-        );
-      });
+    return queryInterface.bulkInsert(
+      "secondLanguageProficiencies",
+      [
+        {
+          readingProficiency: "C",
+          writingProficiency: "B",
+          oralProficiency: "C",
+          readingDate: new Date(),
+          writingDate: new Date(),
+          oralDate: new Date(),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          readingProficiency: "A",
+          writingProficiency: "B",
+          oralProficiency: "C",
+          readingDate: new Date(),
+          writingDate: new Date(),
+          oralDate: new Date(),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
+    // .then(() => {
+    //   return queryInterface.bulkInsert(
+    //     "secondLanguageProficiencies",
+    //     [
+    //       {
+    //         id: "91485e4c-ffdb-11e9-8d71-362b9e155667",
+    //         readingProficiency: "A",
+    //         writingProficiency: "C",
+    //         oralProficiency: "B",
+    //         readingDate: new Date(),
+    //         writingDate: new Date(),
+    //         oralDate: new Date(),
+    //         createdAt: new Date(),
+    //         updatedAt: new Date()
+    //       },
+    //       {
+    //         id: "a34e38b4-ffdb-11e9-8d71-362b9e155667",
+    //         readingProficiency: "A",
+    //         writingProficiency: "B",
+    //         oralProficiency: "A",
+    //         readingDate: new Date(),
+    //         writingDate: new Date(),
+    //         oralDate: new Date(),
+    //         createdAt: new Date(),
+    //         updatedAt: new Date()
+    //       }
+    //     ],
+    //     {}
+    //   );
+    // });
   },
 
   down: (queryInterface, Sequelize) => {
