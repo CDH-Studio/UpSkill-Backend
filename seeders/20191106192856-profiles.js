806f7d54d8f6bca93a2a0cdbd9582e6e6378abc4
Mamadou Bah <bmama067@uottawa.ca>
2019-12-02 10:16:04 -0500

    Fixing seedData

--- seeders/20191106192856-profiles.js
+++ seeders/20191106192856-profiles.js
@@ -30,6 +30,32 @@ module.exports = {
           // talentMatrixResultId: "e4f5d678-ff45-11e9-8f0b-362b9e155667",
           // keyCompetencyId: "ff60e5f6-ff46-11e9-8f0b-362b9e155667",
           // secondLanguageProficiencyId: "a34e38b4-ffdb-11e9-8d71-362b9e155667"
+        },
+        {
+          id: "03030043-a546-4906-9e84-c45d944c78bc",
+          firstName: "John",
+          lastName: "Doe",
+          jobTitleEn: "Manager",
+          jobTitleFr: "Gestionaire",
+          telephone: "343-123-4567",
+          cellphone: "613-123-4567",
+          // locationId: "5888300c-0245-11ea-8d71-362b9e155667",
+          manager: "Chahine El Chaar",
+          team: "CDH Studio",
+          firstLanguage: "Français",
+          secondLanguage: "English",
+          yearService: "23",
+          linkedin: "linkedinUrl",
+          github: "githubUrl",
+          createdAt: new Date(),
+          updatedAt: new Date()
+          // tenureId: "725c6d40-ddc3-4a55-bdf5-f85155091acd",
+          // groupLevelId: "a3975faa-ff43-11e9-8f0b-362b9e155667",
+          // securityClearanceId: "882ef330-ff44-11e9-8f0b-362b9e155667",
+          // careerMobilityId: "ed78c3ba-ff44-11e9-8f0b-362b9e155667",
+          // talentMatrixResultId: "e4f5d678-ff45-11e9-8f0b-362b9e155667",
+          // keyCompetencyId: "ff60e5f6-ff46-11e9-8f0b-362b9e155667",
+          // secondLanguageProficiencyId: "a34e38b4-ffdb-11e9-8d71-362b9e155667"
         }
       ],
       {}
