d45080644540f75d8689ab747e9052c7b75a810e
Mamadou Bah <bmama067@uottawa.ca>
2019-12-02 10:03:30 -0500

    Updated profile-projects

--- seeders/20191107150844-profile-projects.js
+++ seeders/20191107150844-profile-projects.js
@@ -1,3 +1,51 @@
+// "use strict";
+
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//     /*
+//       Add altering commands here.
+//       Return a promise to correctly handle asynchronicity.
+//     */
+//     return queryInterface.bulkInsert(
+//       "profileProjects",
+//       [
+//         {
+//           id: "148b12da-0172-11ea-8d71-362b9e155667",
+//           description: "Level 2 Undergraduate students",
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//           profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
+//         },
+//         {
+//           id: "1d5c73b8-0172-11ea-8d71-362b9e155667",
+//           description: "Collaborative inquiry",
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//           profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
+//         },
+//         {
+//           id: "bce579b6-0172-11ea-8d71-362b9e155667",
+//           description: "O-Train",
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//           profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
+//         }
+//       ],
+//       {}
+//     );
+//   },
+
+//   down: (queryInterface, Sequelize) => {
+//     /*
+//       Add reverting commands here.
+//       Return a promise to correctly handle asynchronicity.
+
+//       Example:
+//       return queryInterface.bulkDelete('People', null, {});
+//     */
+//     return queryInterface.bulkDelete("profileProjects", null, {});
+//   }
+// };
 "use strict";
 
 module.exports = {
@@ -5,34 +53,13 @@ module.exports = {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
     */
-    return queryInterface.bulkInsert(
-      "profileProjects",
-      [
-        {
-          id: "148b12da-0172-11ea-8d71-362b9e155667",
-          description: "Level 2 Undergraduate students",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
-        },
-        {
-          id: "1d5c73b8-0172-11ea-8d71-362b9e155667",
-          description: "Collaborative inquiry",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
-        },
-        {
-          id: "bce579b6-0172-11ea-8d71-362b9e155667",
-          description: "O-Train",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
-        }
-      ],
-      {}
-    );
   },
 
   down: (queryInterface, Sequelize) => {
@@ -43,6 +70,5 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
-    return queryInterface.bulkDelete("profileProjects", null, {});
   }
 };
