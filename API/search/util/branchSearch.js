d25aa1796a4806d62dfaa69a84a2fc602de4378f
Mamadou Bah <bmama067@uottawa.ca>
2019-12-02 15:03:06 -0500

    Fixed branch search

--- API/search/util/branchSearch.js
+++ API/search/util/branchSearch.js
@@ -12,17 +12,3 @@ const branchSearch = async (profiles, branchArray) => {
 };
 
 module.exports = branchSearch;
-
-const locationSearch = async (profiles, locationArray) => {
-  let locationProf = [];
-
-  profiles.forEach(profile => {
-    if (!profile.location) return;
-    const locationId = profile.location.id;
-
-    if (locationArray.includes(locationId)) locationProf.push(profile);
-  });
-  return locationProf;
-};
-
-module.exports = locationSearch;
