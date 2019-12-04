480d2c3b7fb5d4b6c9713ca33ae12a3bf13f2b07
Mamadou Bah <bmama067@uottawa.ca>
2019-12-04 13:24:50 -0500

    Adding backend function for dashboard

---
+++ API/admin/Dashboard/index.js
@@ -0,0 +1,8 @@
+const get = require("../Dashboard/get");
+
+module.exports = {
+  getAllFlaggedProfiles: get.getAllFlaggedProfiles,
+  getAllInactiveUsers: get.getAllInactiveUsers,
+  getAllUsers: get.getAllUsers,
+  getAllExFeeders: getAllExFeeders
+};
