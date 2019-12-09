313cd8bf4874f4fb857457e0281e0509d5ab1398
Mamadou Bah <bmama067@uottawa.ca>
2019-12-09 13:58:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, Authorization, X-Requested-With, Content-Type, Accept"
   );
   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
   next();
