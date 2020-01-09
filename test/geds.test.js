d33aac9bf9fe62bd88744470997f65e33e248c6f
Rizvi <rizvida1@gmail.com>
2020-01-09 15:33:03 -0500

    Added test case with moch req an res

--- test/geds.test.js
+++ test/geds.test.js
@@ -1,5 +1,29 @@
 const geds = require("../API/geds");
 
-test("Expect results from geds", () => {
-  expect(geds.getEmployeeInfo()).toBe(3);
+const mockRequest = data => {
+  return { ...data };
+};
+
+const mockResponse = () => {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
+
+describe("getEmployeeInfo", () => {
+  test("Expect results from geds", async () => {
+    const req = mockRequest({ params: { searchValue: "rizvi" } });
+    const res = mockResponse();
+    await geds.getEmployeeInfo(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalled();
+  });
+
+  test("jest.fn recalls what it has been called with", () => {
+    const mock = jest.fn();
+    mock("a", "b", "c");
+    expect(mock).toHaveBeenCalledTimes(1);
+    expect(mock).toHaveBeenCalledWith("a", "b", "c");
+  });
 });
