ff45fb95270b33af71acf8069b4f324bbfc8aa8e
Rizvi <rizvida1@gmail.com>
2020-01-14 13:59:52 -0500

    Started manual mock

--- test/get.test.js
+++ test/get.test.js
@@ -1,5 +1,6 @@
 const get = require("../API/user/get");
 const Models = require("../models");
+jest.mock("../models");
 
 // Allows jest to track sequelize-mock:
 // jest.mock("../models/user", () => {
@@ -13,26 +14,6 @@ const Models = require("../models");
 // });
 
 // sequelize.$overrideImport('./users/model.js', './users/mock.js');
-// Import the mock library
-var SequelizeMock = require("sequelize-mock");
-
-// Setup the mock database connection
-var DBConnectionMock = new SequelizeMock();
-
-// Define our Model
-var UserMock = DBConnectionMock.define("user", {
-  id: "247ed9ea-30c2-11ea-aee3-af83807df973",
-  name: "Sukhsimranpreet Singh Sekhon",
-  email: "sukhsimranpreetsingh.sekhon@canada.ca",
-  inactive: false
-});
-
-// You can also associate mock models as well
-var GroupMock = DBConnectionMock.define("groups", {
-  name: "My Awesome Group"
-});
-
-UserMock.belongsTo(GroupMock);
 
 const mockRequest = data => {
   const req = {};
@@ -50,15 +31,6 @@ const mockResponse = () => {
 
 jest.mock("../models", () => {
   return { user: jest.fn() };
-});
-
-Models.user.mockImplementation(() => {
-  return DBConnectionMock.define("user", {
-    id: "247ed9ea-30c2-11ea-aee3-af83807df973",
-    name: "Sukhsimranpreet Singh Sekhon",
-    email: "sukhsimranpreetsingh.sekhon@canada.ca",
-    inactive: false
-  });
 });
 
 describe("getUserInfo", () => {
@@ -85,9 +57,4 @@ const mockUserData = {
       inactive: false
     }
   ]
-};
-
-module.exports = {
-  mockRequest,
-  mockResponse
 };
