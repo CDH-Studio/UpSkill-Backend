4e0c761cd5869c2d8a82bb537ae568745c5fd7e2
Rizvi <rizvida1@gmail.com>
2020-01-14 13:59:52 -0500

    Started manual mock

---
+++ test/__mocks__/models.js
@@ -0,0 +1,26 @@
+// Import the mock library
+const SequelizeMock = require("sequelize-mock");
+
+// Setup the mock database connection
+const DBConnectionMock = new SequelizeMock();
+
+// Define our Model
+const UserMock = DBConnectionMock.define("user", {
+  id: "247ed9ea-30c2-11ea-aee3-af83807df973",
+  name: "Sukhsimranpreet Singh Sekhon",
+  email: "sukhsimranpreetsingh.sekhon@canada.ca",
+  inactive: false
+});
+
+// You can also associate mock models as well
+const GroupMock = DBConnectionMock.define("groups", {
+  name: "My Awesome Group"
+});
+
+UserMock.belongsTo(GroupMock);
+
+await UserMock.findAll().then(([data]) => {
+  console.log(data.dataValues);
+});
+
+module.exports = { user: UserMock };
