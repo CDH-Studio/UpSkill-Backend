2c67eb4ff22d71b5a96104346d6ff356e8462da4
Rizvi <rizvida1@gmail.com>
2020-01-14 13:08:01 -0500

    Have working mock user

--- test/get.test.js
+++ test/get.test.js
@@ -1,4 +1,5 @@
 const get = require("../API/user/get");
+const Models = require("../models");
 
 // Allows jest to track sequelize-mock:
 // jest.mock("../models/user", () => {
@@ -12,24 +13,31 @@ const get = require("../API/user/get");
 // });
 
 // sequelize.$overrideImport('./users/model.js', './users/mock.js');
+// Import the mock library
+var SequelizeMock = require("sequelize-mock");
 
-const SequelizeMock = require("sequelize-mock");
+// Setup the mock database connection
+var DBConnectionMock = new SequelizeMock();
 
-const dbMock = new SequelizeMock();
-
-const UserMock = dbMock.define("user", {
-  id: "0a",
-  name: "John Doe",
-  email: "john.doe@canada.ca",
+// Define our Model
+var UserMock = DBConnectionMock.define("user", {
+  id: "247ed9ea-30c2-11ea-aee3-af83807df973",
+  name: "Sukhsimranpreet Singh Sekhon",
+  email: "sukhsimranpreetsingh.sekhon@canada.ca",
   inactive: false
 });
 
-jest.mock("sequelize-mock");
+// You can also associate mock models as well
+var GroupMock = DBConnectionMock.define("groups", {
+  name: "My Awesome Group"
+});
+
+UserMock.belongsTo(GroupMock);
 
 const mockRequest = data => {
   const req = {};
-  req.id = data;
-  //return { ...data };
+  req.params = { id: data };
+  return req;
 };
 
 const mockResponse = () => {
@@ -40,6 +48,19 @@ const mockResponse = () => {
   return res;
 };
 
+jest.mock("../models", () => {
+  return { user: jest.fn() };
+});
+
+Models.user.mockImplementation(() => {
+  return DBConnectionMock.define("user", {
+    id: "247ed9ea-30c2-11ea-aee3-af83807df973",
+    name: "Sukhsimranpreet Singh Sekhon",
+    email: "sukhsimranpreetsingh.sekhon@canada.ca",
+    inactive: false
+  });
+});
+
 describe("getUserInfo", () => {
   it("Should get user from mock", async () => {
     const req = mockRequest("247ed9ea-30c2-11ea-aee3-af83807df973");
@@ -48,9 +69,10 @@ describe("getUserInfo", () => {
 
     //dbMock.mockImplementationOnce(() => Promise.resolve(mockUserData));
 
-    const user = await get.getUserById(req, res);
-
-    expect(user.name).toEqual("Sukhsimranpreet Singh Sekhon");
+    await get.getUserById(req, res).then(data => {
+      console.log(data);
+      expect(data.name).toEqual("Sukhsimranpreet Singh Sekhon");
+    });
   });
 });
 
