ae974d7114b8a4ee9ecda487606750e948c9e549
Sukhu Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>
2020-01-16 18:33:07 -0500

    On-going fix of test cases for get & post

--- API/user/post.js
+++ API/user/post.js
@@ -2,11 +2,13 @@ const Models = require("../../models");
 const User = Models.user;
 
 const createUser = async (request, response) => {
-  User.findOrCreate({
+  // console.log("I am running createUser()");
+  await User.findOrCreate({
     where: { email: request.body.email },
     defaults: { name: request.body.name }
   }).then(async ([user, created]) => {
     let hasProfile = !((await user.getProfile()) == null);
+    console.log("THIS SHOULD BE NULL: ", await user.getProfile(), "The end");
     let resData = { user, created, hasProfile };
     response.status(200).json(resData);
   });
