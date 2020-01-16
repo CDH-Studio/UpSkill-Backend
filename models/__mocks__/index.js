4434506ce9c0710e09577cba695e5d9115a0e36b
Sukhu Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>
2020-01-16 11:04:13 -0500

    Added findAll tests & fixed findOne tests

--- models/__mocks__/index.js
+++ models/__mocks__/index.js
@@ -5,12 +5,26 @@ const SequelizeMock = require("sequelize-mock");
 const DBConnectionMock = new SequelizeMock();
 
 // Define our Model
-const UserMock = DBConnectionMock.define("user", {
-  id: "247ed9ea-30c2-11ea-aee3-af83807df973",
-  name: "Sukhsimranpreet Singh Sekhon",
-  email: "sukhsimranpreetsingh.sekhon@canada.ca",
-  inactive: false
-});
+const UserMock = DBConnectionMock.define("user", [
+  {
+    id: "b0d410be-30c1-11ea-89a8-d3732f9abb69",
+    name: "John Doe",
+    email: "john.doe@canada.ca",
+    inactive: false
+  },
+  {
+    id: "faba08aa-ffe3-11e9-8d71-362b9e155667",
+    name: "Mary Doe",
+    email: "mary.doe@canada.ca",
+    inactive: false
+  },
+  {
+    id: "247bc9za-30d2-11wq-dff3-a8j3807ty973",
+    name: "Sir Bobby Hall",
+    email: "bobby.hall@canada.ca",
+    inactive: true
+  }
+]);
 
 // You can also associate mock models as well
 const GroupMock = DBConnectionMock.define("groups", {
