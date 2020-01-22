e3f56eb5dc810ce272530facabfe73fae6f2cc86
Sukhu Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>
2020-01-22 15:03:39 -0500

    Continued work on countCompetencyProfiles.js --- WIP

--- API/admin/reporting/get.js
+++ API/admin/reporting/get.js
@@ -3,7 +3,8 @@
 const utils = require("./util");
 
 const statistics = async (request, response) => {
-  let results = await utils.countSkillProfiles().then(res => res);
+  // let results = await utils.countSkillProfiles().then(res => res);
+  let results = await utils.countCompetencyProfiles().then(res => res);
 
   response.status(200).json(results);
 };
