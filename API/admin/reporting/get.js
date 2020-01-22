678264231d0c24ffda67de1687252c61348e04b3
Rizvi <rizvida1@gmail.com>
2020-01-22 17:15:19 -0500

    Fixed skill count

--- API/admin/reporting/get.js
+++ API/admin/reporting/get.js
@@ -3,9 +3,9 @@
 const utils = require("./util");
 
 const statistics = async (request, response) => {
-  // let results = await utils.countSkillProfiles().then(res => res);
-  let results = await utils.countCompetencyProfiles().then(res => res);
+  let skillCount = await utils.countSkillProfiles().then(res => res);
+  let compCount = await utils.countCompetencyProfiles().then(res => res);
 
-  response.status(200).json(results);
+  response.status(200).json({ skillCount: skillCount, compCount: compCount });
 };
 module.exports = { statistics };
--- API/admin/reporting/util/countCompetencyProfiles.js
+++ API/admin/reporting/util/countCompetencyProfiles.js
@@ -3,27 +3,47 @@ const Models = require("../../../../models");
 const Skills = Models.skill; // Skills Table
 const Profiles = Models.profile; // Profiles Table
 
+const sequelize = Models.sequelize;
+
 const countCompetencyProfiles = async () => {
-  const profileCompetencies = await Skills.findAll({
-    // (Competency ID (id), descriptionEn, descriptionFr, Count Profile Occurences (countOccurences)}
-    group: ["skill.id"],
-    includeIgnoreAttributes: false,
-    where: { type: "competency" },
-    include: [
-      {
-        model: Profiles,
-        attributes: ["id"]
-      }
-    ],
-    attributes: [
-      "id",
-      "descriptionEn",
-      "descriptionFr",
-      [Sequelize.fn("COUNT", Sequelize.col("profiles.id")), "countOccurences"]
-    ],
-    order: [[Sequelize.fn("COUNT", Sequelize.col("profiles.id")), "DESC"]]
-  });
-  return profileCompetencies;
+  // const profileCompetencies = await Skills.findAll({
+  //   // (Competency ID (id), descriptionEn, descriptionFr, Count Profile Occurences (countOccurences)}
+  //   group: ["skill.id"],
+  //   includeIgnoreAttributes: false,
+  //   where: { type: "competency" },
+  //   include: {
+  //     association: "competencies",
+  //     attributes: ["id"]
+  //   },
+  //   attributes: [
+  //     "id",
+  //     "descriptionEn",
+  //     "descriptionFr",
+  //     [Sequelize.fn("COUNT", Sequelize.col("profiles.id")), "countOccurences"]
+  //   ],
+  //   order: [[Sequelize.fn("COUNT", Sequelize.col("profiles.id")), "DESC"]]
+  // });
+
+  const profileCompetencies = await sequelize.query(
+    `SELECT
+        "skill"."id",
+        "skill"."descriptionEn",
+        "skill"."descriptionFr",
+        COUNT("profiles"."id") AS "countOccurences"
+      FROM "skills" AS "skill"
+      LEFT OUTER JOIN (
+          "profileCompetencies" AS "profiles->profileCompetencies"
+          INNER JOIN "profiles" AS "profiles" ON "profiles"."id" = "profiles->profileCompetencies"."profileId"
+        ) ON "skill"."id" = "profiles->profileCompetencies"."skillId"
+      WHERE
+        "skill"."type" = 'competency'
+      GROUP BY
+        "skill"."id"
+      ORDER BY
+        COUNT("profiles"."id") DESC;`
+  );
+
+  return profileCompetencies[0];
 };
 
 module.exports = countCompetencyProfiles;
