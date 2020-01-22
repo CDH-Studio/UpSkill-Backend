a824a0787c5b31ceb45b8c1c85c4068e9c75fd7a
Sukhu Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>
2020-01-22 10:47:28 -0500

    Update countSkillsProfiles --- WIP

--- API/admin/reporting/util/countSkillProfiles.js
+++ API/admin/reporting/util/countSkillProfiles.js
@@ -1,26 +1,38 @@
 const Models = require("../../../../models");
 
+const Migration = require("../../../../migrations/20191101141536-add-skill-associations");
+
 const Skills = Models.skill; // skill.js
-const ProfileSkills = Models.profileSkills; // profileSkills.js
+const ProfileSkills = Migration[0].up;
 
 const countSkillProfiles = async () => {
   const occurences = {}; // {descriptionEn, descriptionFr, profileCount}
 
-  const getRows = await Skills.findAll();
+  const getRows = await Skills.findAll({
+    where: {
+      type: "skill"
+    }
+  });
 
   // console.log("Output: ", getRows);
 
   getRows.forEach(row => {
     const descriptionEn = row.descriptionEn;
-    console.log("Skill: ", descriptionEn);
+
+    // console.log("Skill: ", descriptionEn);
+
     const descriptionFr = row.descriptionFr;
 
-    const count = ProfileSkills.count({ where: { skillId: row.id } });
+    const id = row.id;
 
-    occurences.push([descriptionEn, descriptionFr, count]);
+    // console.log("Skill ID: ", id);
+
+    const amount = ProfileSkills.count({ where: { skillId: id } });
+
+    occurences.push([descriptionEn, descriptionFr, amount]);
   });
 
-  //console.log("Occurences", occurences);
+  // console.log("Occurences", occurences);
   return occurences;
 };
 
