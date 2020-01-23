1e665e3565ef5d61c4933b4ad3a50d34a7a8ae52
Sukhu Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>
2020-01-23 11:09:13 -0500

    Added countDevelopmentGoals.js

--- API/admin/reporting/util/countCompetencyProfiles.js
+++ API/admin/reporting/util/countCompetencyProfiles.js
@@ -1,8 +1,6 @@
 const Models = require("../../../../models");
 const sequelize = Models.sequelize;
 
-const sequelize = Models.sequelize;
-
 const countCompetencyProfiles = async () => {
   const profileCompetencies = await sequelize.query(
     `SELECT
