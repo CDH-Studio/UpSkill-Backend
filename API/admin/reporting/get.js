b11ad1a789e98730dc22fece456e5b5143fa9b60
Sukhu Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>
2020-01-23 16:30:38 -0500

    Added flaggedProfiles.js

--- API/admin/reporting/get.js
+++ API/admin/reporting/get.js
@@ -6,8 +6,10 @@ const statistics = async (request, response) => {
   let skillCount = await utils.countSkillProfiles().then(res => res);
   let compCount = await utils.countCompetencyProfiles().then(res => res);
   let developCount = await utils.countDevelopmentGoals().then(res => res);
+  let flaggedProfiles = await utils.flaggedProfiles().then(res => res);
 
   response.status(200).json({
+    flaggedProfiles: flaggedProfiles,
     developCount: developCount,
     skillCount: skillCount,
     compCount: compCount
