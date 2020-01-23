e69e6f6bbbb163ac2da98ddf4ec8708a9234915f
Sukhu Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>
2020-01-23 11:09:13 -0500

    Added countDevelopmentGoals.js

--- API/admin/reporting/get.js
+++ API/admin/reporting/get.js
@@ -5,7 +5,12 @@ const utils = require("./util");
 const statistics = async (request, response) => {
   let skillCount = await utils.countSkillProfiles().then(res => res);
   let compCount = await utils.countCompetencyProfiles().then(res => res);
+  let developCount = await utils.countDevelopmentGoals().then(res => res);
 
-  response.status(200).json({ skillCount: skillCount, compCount: compCount });
+  response.status(200).json({
+    developCount: developCount,
+    skillCount: skillCount,
+    compCount: compCount
+  });
 };
 module.exports = { statistics };
---
+++ API/admin/reporting/util/countDevelopmentGoals.js
@@ -0,0 +1,29 @@
+// const Sequelize = require("sequelize");
+const Models = require("../../../../models");
+// const Skills = Models.skill; // Skills Table
+// const Profiles = Models.profile; // Profiles Table
+
+const sequelize = Models.sequelize;
+
+const countDevelopmentGoals = async () => {
+  const profileDevelopmentGoals = await sequelize.query(
+    `SELECT 
+          "skill"."id",
+          "skill"."descriptionEn",
+          "skill"."descriptionFr",
+          COUNT("profiles"."id") AS "countOccurences"
+        FROM "skills" AS "skill"
+        LEFT OUTER JOIN (
+              "profileDevelopmentGoals" AS "profiles->profileDevelopmentGoals"
+              INNER JOIN "profiles" as "profiles" ON "profiles"."id" = "profiles->profileDevelopmentGoals"."profileId"
+            ) ON "skill"."id" = "profiles->profileDevelopmentGoals"."skillId"
+        GROUP BY
+            "skill"."id"
+        ORDER BY
+            COUNT("profiles"."id") DESC;`
+  );
+
+  return profileDevelopmentGoals[0];
+};
+
+module.exports = countDevelopmentGoals;
