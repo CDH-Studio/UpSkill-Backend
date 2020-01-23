b6064dd5d055ae8d5f6b84b5bad135e7f0a3b714
Sukhu Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>
2020-01-23 16:30:38 -0500

    Added flaggedProfiles.js

--- API/admin/reporting/util/index.js
+++ API/admin/reporting/util/index.js
@@ -1,9 +1,11 @@
 const countSkillProfiles = require("./countSkillProfiles");
 const countCompetencyProfiles = require("./countCompetencyProfiles");
 const countDevelopmentGoals = require("./countDevelopmentGoals");
+const flaggedProfiles = require("./flaggedProfiles");
 
 module.exports = {
   countSkillProfiles,
   countCompetencyProfiles,
-  countDevelopmentGoals
+  countDevelopmentGoals,
+  flaggedProfiles
 };
