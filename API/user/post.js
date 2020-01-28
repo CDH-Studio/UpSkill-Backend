3051696d853d57865a7f6081054ec1cb0bdb4db8
Sukhu Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>
2020-01-28 13:20:48 -0500

    Cleared console.log() statements from unit tests

--- API/user/post.js
+++ API/user/post.js
@@ -6,7 +6,6 @@ const createUser = async (request, response) => {
     where: { email: request.body.email },
     defaults: { name: request.body.name }
   }).then(async ([user, created]) => {
-    console.log("THIS SHOULD BE NULL: ", await user.getProfile(), "The end");
     let hasProfile = !((await user.getProfile()) == null);
     let resData = { user, created, hasProfile };
     response.status(200).json(resData);
